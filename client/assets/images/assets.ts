253f1ab742a7b9b8cb760100ed01fa8a9ccaed63
Tsiory Jonathan <167012196+TsioryJonathan@users.noreply.github.com>
2025-08-16 10:08:39 +0300

    Feat/login page (#2)
    * feat: basic login page
    
    * style: update logo image
    
    * refactor: update project structure by separating backend and frontend
    
    * feat: welcome page and login form
    
    * fix: remove unused import
    
    * style: fix sizing
    
    * style: improve auth page design

---
+++ client/assets/images/assets.ts
@@ -0,0 +1,11 @@
+import logo from "./logo.png";
+import deliveryGuyMockup from "./delivery.png";
+import deliveryGuy from "./deliveryGuy.png";
+import tana from "./tana.png";
+
+export const assets = {
+  logo,
+  deliveryGuyMockup,
+  deliveryGuy,
+  tana,
+};
