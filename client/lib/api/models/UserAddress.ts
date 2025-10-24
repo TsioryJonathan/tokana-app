b5df3f6357983ca60c8b75aae0af5c90f02c9f0f
Mathieu <hei.tafita.2@gmail.com>
2025-10-24 16:41:01 +0300

    chore(sdk): régénérer client (models/UserAddress)

---
+++ client/lib/api/models/UserAddress.ts
@@ -0,0 +1,16 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type UserAddress = {
+    id?: number;
+    userId?: number;
+    label?: string | null;
+    detail?: string;
+    lat?: number | null;
+    lng?: number | null;
+    isDefault?: boolean;
+    createdAt?: string;
+    updatedAt?: string;
+};
+
